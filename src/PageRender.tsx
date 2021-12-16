38742e478d98d86da5ceb2bf523481e326c1891c
emreMilon <kurt@milon.com>
2021-12-16 09:37:19 +0100

    setup: project initialized

---
+++ src/PageRender.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { IParams } from './utils/TypeScript'
+import NotFound from './components/global/NotFound'
+
+
+const generatePage = (name: string) => {
+  const component = () => require(`./pages/${name}`).default
+
+  try {
+    return React.createElement(component())
+  } catch (err) {
+    return <NotFound />;
+  }
+}
+
+const PageRender = () => {
+  const { page, id }: IParams = useParams()
+
+  let name = '';
+
+  if(page){
+    name = id ? `${page}/[id]` : `${page}`
+  }
+
+  return generatePage(name)
+}
+
+export default PageRender
